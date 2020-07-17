6b4340f3f9e7b5c364dbef2f22f8e5345227e30a
Chaitali Bochare <chaitalibochare2283@gmail.com>
2020-07-17 22:43:47 +0530

    Updates

---
+++ precache-manifest.57e1e22f190ca14482333e16350bd485.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e52be2d37675a13e1800fe3e0460ea2e",
+    "url": "/searchrecipe/index.html"
+  },
+  {
+    "revision": "bab34425473168e8ce1f",
+    "url": "/searchrecipe/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "25f683e7b29312685cf0",
+    "url": "/searchrecipe/static/css/main.93fab01c.chunk.css"
+  },
+  {
+    "revision": "bab34425473168e8ce1f",
+    "url": "/searchrecipe/static/js/2.8f5970bd.chunk.js"
+  },
+  {
+    "revision": "9e81e617707513432f3da640fbf3bd82",
+    "url": "/searchrecipe/static/js/2.8f5970bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25f683e7b29312685cf0",
+    "url": "/searchrecipe/static/js/main.77dde369.chunk.js"
+  },
+  {
+    "revision": "f8975a904477dec5e383",
+    "url": "/searchrecipe/static/js/runtime-main.c08324e4.js"
+  },
+  {
+    "revision": "880f228b991fdc18f4e6930f009f81c2",
+    "url": "/searchrecipe/static/media/bg1.880f228b.jpg"
+  }
+]);
